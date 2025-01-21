3f7093f8fe9cb76a911c42bf5a8f7122e53c828c
Kurt Seifried <kurt@seifried.org>
2025-01-21 16:09:13 -0700

    updates

--- urls/get-urls-glama-ai.sh
+++ urls/get-urls-glama-ai.sh
@@ -4,5 +4,5 @@ curl https://glama.ai/mcp/servers.json| jq -r '.servers[].githubUrl' > urls-glam
 
 # Find any new URLs
 
-# cat github* urls-glama-ai.txt urls-glama-ai.txt| sort | uniq -c | grep " 2 " | grep githu | sed 's/^   2 /.\/MCP-Mirror\/utils\/mirror-github-repos-to-MCP-Mirror.sh --directory \/Users\/kurt\/GitHub\/MCP-Mirror --repo /' > ../../script.sh
+cat github* urls-glama-ai.txt urls-glama-ai.txt| sort | uniq -c | grep " 2 " | grep githu | sed 's/^   2 /.\/MCP-Mirror\/utils\/mirror-github-repos-to-MCP-Mirror.sh --directory \/Users\/kurt\/GitHub\/MCP-Mirror --repo /' > ../../script.sh
 
